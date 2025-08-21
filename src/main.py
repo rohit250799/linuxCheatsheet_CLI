ea48b22b211d9215bddd1a09658f84a65cd9bd00
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-08-21 08:39:14 +0530

    fix: particular input command description entered as argument in CLI and remove unnecessary arguments

--- src/main.py
+++ src/main.py
@@ -3,8 +3,7 @@ import argparse
 parser = argparse.ArgumentParser(prog='Linux CLI cheatsheet', description='display a linux command cheatsheet')
 parser.add_argument("commands", default=argparse.SUPPRESS, help="display all the available linux commands available in the linux cheatsheet")
 parser.add_argument("-v", "--verbose", action="store_true", help="increases output verbosity")
-parser.add_argument("commandDescription", help="describe the particular command provided as an argument")
-parser.add_argument("commandInput", help="take the command as input, to describe it")
+parser.add_argument("-cdes","--commandDescription", help="describe the particular command provided as an argument")
 args = parser.parse_args()
 
 commands_map = {
@@ -36,13 +35,13 @@ def display_all_commands_verbose() -> None:
     
     return
 
-if args.commands and not args.verbose:
+if args.commands and not args.verbose and not args.commandDescription:
     display_all_commands()    
 
 if args.verbose:
     display_all_commands_verbose()
     print("Since verbosity was been turned on")
 
-if args.commandDescription and args.commandInput:
-    if args.commands: display_command_description(command_input=args.commandInput)
-    else: SystemExit.code(2)
+if args.commandDescription:
+    display_command_description(args.commandDescription)
+    print('Description text displayed in the above line')
