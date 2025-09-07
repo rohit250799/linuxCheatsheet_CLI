4c4ed8d11f326b01926e2334788eb0ffccfbe181
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-09-07 21:29:01 +0530

    modify: suggest_command function to split user input into words and check for their existence in description list

--- src/search_appropriate_commands.py
+++ src/search_appropriate_commands.py
@@ -20,18 +20,36 @@ def suggest_command(user_query_input: str) -> None:
     yaml_data: dict = import_data_from_yaml_file()
     suggestion_found = None
     command_suggestions = []
+    user_query_input_words_list = user_query_input.split()
+    all_commands_description_list = []
+    all_commands_categories_list = []
 
 
     for command, details in yaml_data.items():
         command_description: str = details['description'].lower()
         command_category: str = details['category']
-        suggestion_found = re.findall(user_query_input.lower(), command_description) or re.findall(user_query_input, command_category) or re.findall(user_query_input, command)
-        if suggestion_found: command_suggestions.append(command) 
+        all_commands_description_list.append(command_description)
+        all_commands_categories_list.append(command_category)
+        # suggestion_found = re.findall(user_query_input.lower(), command_description) or re.findall(user_query_input, command_category) or re.findall(user_query_input, command)
+        # if suggestion_found: command_suggestions.append(command) 
+
+    for query_word in user_query_input_words_list:
+        for single_command_description in all_commands_description_list:
+            if query_word in single_command_description:
+                print(f'The current single command description is: {single_command_description}') 
+                command_to_display = all_commands_description_list.index(single_command_description)
+                command_suggestions.append(command_to_display)
+        # if query_word in all_commands_categories_list or query_word in all_commands_description_list: 
+        #     command_to_display = all_commands_description_list(command_description)
+        #     command_suggestions.append(command_to_display) 
             
         
     if command_suggestions: 
         print(f'Suggestions have been found: {command_suggestions}')
+        print(f'{all_commands_description_list} is the all commands description list')
         return
     
-    print('A suggestion could not be found')    
+    print('A suggestion could not be found')
+    print(f'{user_query_input_words_list} is user query input words list')
+    print(f'{all_commands_description_list} is all commands description list')    
     return suggestion_found
