1aabad17bdcc56b9d6c7092980d46dff52d24783
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-09-10 14:47:48 +0530

    modify: suggestions command in cheatsheet to search for the user input using subprocess and man commands on user's choice

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 import argparse
 import subprocess
+import sys
 
 from load_from_yaml import display_all_command_categories_num, display_all_command_num, display_all_available_commands_in_cheatsheet, display_all_available_categories_in_cheatsheet
 from search_appropriate_commands import search_command_by_name, search_for_commands
+from subprocesses_management import search_command_using_man
 
 parser = argparse.ArgumentParser(prog='Linux CLI cheatsheet', description='display a linux command cheatsheet')
 subparsers = parser.add_subparsers(dest='myCommand', required=True)
@@ -40,6 +42,15 @@ elif args.myCommand == 'categories':
 
 elif args.myCommand == 'suggestions':
     searching_result = search_for_commands(user_query_input=args.s)
-    print(searching_result) if searching_result else print(f'No suggestions have been found for your query: {args.s}')
-    print('\n The required data has been displayed above')
-
+    if searching_result: 
+        print(searching_result)
+        sys.exit(0) 
+    else: 
+        print(f'No suggestions have been found for your query: {args.s} \n')
+        customer_continuation_input = int(input("Press 0 if you want to exit and 1 if you want to search for the command online: "))
+        if customer_continuation_input == 1:
+            search_command_using_man(args.s)
+            print('All the suggested commands are displayed above')
+        else:
+            print('You have exit the command line!')
+            sys.exit(0)
