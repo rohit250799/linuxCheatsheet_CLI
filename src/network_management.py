d5b6a07efb3a907265992a5947d1742e2ca3ec02
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-09-21 16:21:33 +0530

    add: function to see if network id is type v4 or v6

--- src/network_management.py
+++ src/network_management.py
@@ -1,5 +1,10 @@
 from subprocesses_management import display_current_ip_address
-# import ipaddress
+import ipaddress
+
+def find_v4_or_v6(network_id: str) -> str:
+    if isinstance(ipaddress.ip_network(network_id), ipaddress.IPv4Network): return 'v4'
+    elif isinstance(ipaddress.ip_network(network_id), ipaddress.IPv6Network): return 'v6'
+    else: raise ValueError('An incorrect value has been ised') 
 
 def find_network_id_class(network_id) -> str:
     network_id_first_octet_list:list = []
@@ -20,11 +25,11 @@ def calculate_free_bits_for_network(input_subnet_mask: int) -> int:
     print(f'The entered subnet mask is: {input_subnet_mask}')
     return 32 - input_subnet_mask
 
-def calculate_total_hosts_for_each_subnet(subnet_number: int, free_bits: int = calculate_free_bits_for_network()):
-    total_addresses = 2 ** free_bits
-    hosts_in_each_subnet = total_addresses / subnet_number
+# def calculate_total_hosts_for_each_subnet(subnet_number: int, free_bits: int = calculate_free_bits_for_network()):
+#     total_addresses = 2 ** free_bits
+#     hosts_in_each_subnet = total_addresses / subnet_number
 
-    return hosts_in_each_subnet
+#     return hosts_in_each_subnet
 
 def calculate_all_new_network_ids():
     pass
