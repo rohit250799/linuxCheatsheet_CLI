a8f7cf526b7a767ef86d91a4fda3d2699d40fbaf
Rohit Sarkar <rohitsarkar034@gmail.com>
2025-09-29 23:24:38 +0530

    WIP: my local ignorable changes 2

---
+++ tests/test_loading_from_yaml.py
@@ -0,0 +1,9 @@
+import unittest
+
+from src.load_from_yaml import import_data_from_yaml_file
+
+class TestLoadingData(unittest.TestCase):
+    # def test_importing_data_from_yaml_file(self):
+    #   data_imported = import_data_from_yaml_file()
+    #   self.assertEqual()
+    pass
